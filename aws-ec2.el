ed56389742286a94e53697161616f8420ca4c59f
Yuki Inoue <inouetakahiroki@gmail.com>
2016-10-02 18:54:16 +0900

    Separate tblui

--- aws-ec2.el
+++ aws-ec2.el
@@ -5,7 +5,7 @@
 ;; Author: Yuki Inoue <inouetakahiroki _at_ gmail.com>
 ;; URL: https://github.com/Yuki-Inoue/aws.el
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.4") (dash "2.12.1") (magit-popup "2.6.0") (tablist "0.70"))
+;; Package-Requires: ((emacs "24.4") (dash "2.12.1") (magit-popup "2.6.0") (tablist "0.70") (cl-lib "0.5"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -29,9 +29,8 @@
 ;;; Code:
 
 (require 'json)
-(require 'tablist)
 (require 'dash)
-(require 'magit-popup)
+(require 'tblui)
 
 ;;;###autoload
 (defcustom aws-command (executable-find "aws")
@@ -96,115 +95,38 @@
       (setq profile nil))
   (setq aws-current-profile profile))
 
-(define-derived-mode aws-instances-mode tabulated-list-mode "Containers Menu"
-  "Major mode for handling a list of docker containers."
+(defun aws-instances-get-tabulated-list-entries ()
+  (->>
+   (aws-ec2-normalize-raw-instances
+    (aws-ec2-all-raw-instances))
+   (mapcar (lambda (instance)
+             (list (cdr (assoc 'InstanceId instance))
+                   (vector (assoc-default 'InstanceId instance)
+                           (assoc-default 'InstanceType instance)
+                           (or (assoc-default "Name" (assoc-default 'Tags instance)) "")
+                           (assoc-default 'Name (assoc-default 'State instance))
+                           (prin1-to-string (assoc-default 'PrivateIpAddress instance) t)
+                           (or  "..." (prin1-to-string instance))
+                           ))))
+   )
+  )
 
-  (define-key aws-instances-mode-map "I" #'aws-instances-inspect-popup)
-  (define-key aws-instances-mode-map "S" #'aws-instances-state-popup)
-  (define-key aws-instances-mode-map "A" #'aws-instances-action-popup)
-  (define-key aws-instances-mode-map "C" #'aws-instances-configure-popup)
-  (define-key aws-instances-mode-map "P" #'aws-set-profile)
+(defun aws-instances-reboot-instances (ids)
+  (apply #'aws--shell-command-to-string "ec2" "reboot-instances" "--instance-ids" ids))
 
-  (setq tabulated-list-format
-        '[("Repository" 10 nil)
-          ("InstType" 10 nil)
-          ("Name" 30 nil)
-          ("Status" 10 nil)
-          ("IP" 15 nil)
-          ("Settings" 20 nil)])
-  (setq tabulated-list-padding 2)
-  (add-hook 'tabulated-list-revert-hook #'aws-instances-refresh nil t)
-  (tabulated-list-init-header)
-  (tablist-minor-mode))
+(defun aws-instances-stop-instances (ids)
+  (apply #'aws--shell-command-to-string "ec2" "stop-instances" "--instance-ids" ids))
 
-
-(defun aws-instances-refresh ()
-  "Refresh aws-instances entries."
-
-  (setq
-   tabulated-list-entries
-   (->>
-    (aws-ec2-normalize-raw-instances
-     (aws-ec2-all-raw-instances))
-    (mapcar (lambda (instance)
-              (list (cdr (assoc 'InstanceId instance))
-                    (vector (assoc-default 'InstanceId instance)
-                            (assoc-default 'InstanceType instance)
-                            (or (assoc-default "Name" (assoc-default 'Tags instance)) "")
-                            (assoc-default 'Name (assoc-default 'State instance))
-                            (prin1-to-string (assoc-default 'PrivateIpAddress instance) t)
-                            (or  "..." (prin1-to-string instance))
-                            ))))
-    )))
-
-(defun aws-select-if-empty (&optional arg)
-  "Select current row is selection is empty."
-  (save-excursion
-    (when (null (tablist-get-marked-items))
-      (tablist-put-mark))))
-
-(defmacro aws-define-popup (name doc &rest args)
-  "Wrapper around `aws-utils-define-popup'."
-  `(progn
-     (magit-define-popup ,name ,doc ,@args)
-     (add-function :before (symbol-function ',name) #'aws-select-if-empty)))
-
-(aws-define-popup
- aws-instances-inspect-popup
- 'aws-instances-popups
- :actions  '((?I "Inspect" aws-instances-inspect-selection)))
-
-(aws-define-popup
- aws-instances-state-popup
- 'aws-instances-popups
- :actions  '((?O "Stop" aws-instances-stop-selection)
-             (?R "Reboot" aws-instances-reboot-selection)
-             (?T "Terminate" aws-instances-terminate-selection)
-             (?S "Start" aws-instances-start-selection)))
-
-(aws-define-popup
- aws-instances-action-popup
- 'aws-instances-popups
- :actions  '((?R "Rename Instance" aws-instances-rename-instance)))
-
-(aws-define-popup
- aws-instances-configure-popup
- 'aws-instances-popups
- :actions  '((?C "Append ssh configs to ~/.ssh/config" aws-instances-configure-ssh-config)))
-
-
-
-(defun aws-ec2-command-on-selection (command)
-  (apply 'aws--shell-command-to-string
-         "ec2" command "--instance-ids"
-         (mapcar #'car (tablist-get-marked-items))))
-
-(defun aws-instances-reboot-selection ()
-  (interactive)
-  (aws-ec2-command-on-selection "reboot-instances"))
-
-(defun aws-instances-stop-selection ()
-  (interactive)
-  (aws-ec2-command-on-selection "stop-instances"))
-
-(defun aws-instances-terminate-selection ()
-  (interactive)
+(defun aws-instances-terminate-instances (ids)
   (if (yes-or-no-p (format "Really Terminate the %d instances?"
-                           (length
-                            (tablist-get-marked-items))))
-      (aws-ec2-command-on-selection "terminate-instances")))
+                           (length ids)))
+      (apply #'aws--shell-command-to-string "ec2" "terminate-instances" "--instance-ids" ids)))
 
-(defun aws-instances-start-selection ()
-  (interactive)
-  (aws-ec2-command-on-selection "start-instances"))
+(defun aws-instances-start-instances (ids)
+  (apply #'aws--shell-command-to-string "ec2" "start-instances" "--instance-ids" ids))
 
-(defun aws-instances-inspect-selection ()
-  (interactive)
-  (let ((args (->>
-               (tablist-get-marked-items)
-               (mapcar #'car)
-               (append '("ec2" "describe-instances" "--instance-ids")))))
-    (apply #'aws--shell-command-to-result-buffer args)))
+(defun aws-instances-inspect-instances (ids)
+  (apply #'aws--shell-command-to-result-buffer "ec2" "describe-instances" "--instance-ids" ids))
 
 (defun aws--shell-command-to-result-buffer (&rest args)
   (let ((result (apply #'aws--shell-command-to-string args))
@@ -216,16 +138,14 @@
       (insert result))
     (display-buffer buffer)))
 
-(defun aws-instances-rename-instance ()
-  (interactive)
-  (let ((ids (mapcar #'car (tablist-get-marked-items))))
-    (if (/= 1 (length ids))
-        (error "Multiple instances cannot be selected.")
-      (let ((new-name (read-string "New Name: "))
-            (id (nth 0 ids)))
-        (aws--shell-command-to-string
-         "ec2" "create-tags"
-         "--resources" id "--tags" (format "Key=Name,Value=%s" new-name))))))
+(defun aws-instances-rename-instance (ids)
+  (if (/= 1 (length ids))
+      (error "Multiple instances cannot be selected.")
+    (let ((new-name (read-string "New Name: "))
+          (id (nth 0 ids)))
+      (aws--shell-command-to-string
+       "ec2" "create-tags"
+       "--resources" id "--tags" (format "Key=Name,Value=%s" new-name)))))
 
 (defconst aws-instances-ssh-config-entry-template
   "
@@ -250,12 +170,11 @@ Host %s
   :type '(repeat string)
   )
 
-(defun aws-instances-configure-ssh-config ()
+(defun aws-instances-configure-ssh-config (ids)
   (interactive)
   (let ((aws-instances
          (->>
-          (tablist-get-marked-items)
-          (mapcar #'car)
+          ids
           (apply #'aws--shell-command-to-string
                  "ec2" "describe-instances"
                  "--instance-ids")
@@ -283,14 +202,40 @@ Host %s
           (write-region snippet nil "~/.ssh/config" 'append)
           )))))
 
+(tblui-define
+ aws-instances
+ aws-instances-get-tabulated-list-entries
+ [("Repository" 10 nil)
+  ("InstType" 10 nil)
+  ("Name" 30 nil)
+  ("Status" 10 nil)
+  ("IP" 15 nil)
+  ("Settings" 20 nil)]
+ (((:key . "I")
+   (:name . aws-instances-inspect-popup)
+   (:funcs . ((?I "Inspect" aws-instances-inspect-instances))))
+
+  ((:key . "S")
+   (:name . aws-instances-state-popup)
+   (:funcs . ((?O "Stop" aws-instances-stop-instances)
+              (?R "Reboot" aws-instances-reboot-instances)
+              (?T "Terminate" aws-instances-terminate-instances)
+              (?S "Start" aws-instances-start-instances))))
+
+  ((:key . "A")
+   (:name . aws-instances-action-popup)
+   (:funcs . ((?R "Rename Instance" aws-instances-rename-instance))))
+
+  ((:key . "C")
+   (:name . aws-instances-configure-popup)
+   (:funcs . ((?C "Append ssh configs to ~/.ssh/config" aws-instances-configure-ssh-config))))
+  ))
+
 ;;;###autoload
 (defun aws-instances ()
   "List aws instances using aws-cli. (The `aws` command)."
   (interactive)
-  (pop-to-buffer "*aws-instances*")
-  (tabulated-list-init-header)
-  (aws-instances-mode)
-  (tabulated-list-revert))
+  (aws-instances-goto-ui))
 
 (defvar aws-global-keymap
   (let ((map (make-sparse-keymap)))
